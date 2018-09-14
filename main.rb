3c21482a07b21acb4dc2e6fb8dd9b82c3eef5cd1
fernando amezcua alcantar <fernandoamezcuaalcantar@MacBook-Pro-de-fernando.local>
2018-09-14 14:16:28 -0500

    agregando validaciones

--- main.rb
+++ main.rb
@@ -24,10 +24,14 @@ my_array = Array.new(size)
 my_reference = [[1,2,3],[4,5,6],[7,8,9]]
 
 # Armamos array de referencia
-for i in 0..size
-    print my_reference[i] 
-    puts "\n"
+def tableroReferencia(size, my_reference)
+    for i in 0..size
+        print my_reference[i] 
+        puts "\n"
+    end 
 end 
+
+tableroReferencia(size, my_reference)
 
 # Llenamos matriz con 0
 for m in 0..size
@@ -47,6 +51,83 @@ def getPosition(position, my_reference)
     end
 end
 
+def evaluarGanador(jugador, my_array)
+    
+    # EVALUAR GANADOR HORIZONTAL
+    # Linea 1
+    if my_array[0][0] != 0 && my_array[0][1] != 0 && my_array[0][2] != 0
+        if my_array[0][0] == my_array[0][1] &&  my_array[0][1] == my_array[0][2]
+            return jugador
+        end
+    end
+
+    # Linea 2
+    if my_array[1][0] != 0 && my_array[1][1] != 0 && my_array[1][2] != 0
+        if my_array[1][0] == my_array[1][1] &&  my_array[1][1] == my_array[1][2]
+            return jugador
+        end
+    end
+
+    # Linea 3
+    if my_array[2][0] != 0 && my_array[2][1] != 0 && my_array[2][2] != 0
+        if my_array[2][0] == my_array[2][1] &&  my_array[2][1] == my_array[2][2]
+            return jugador
+        end
+    end
+
+    # VALIDAR EN VERTICAL
+    # Linea 1
+    if my_array[0][0] != 0 && my_array[1][0] != 0 && my_array[2][0] != 0
+        if my_array[0][0] == my_array[1][0] &&  my_array[1][0] == my_array[2][0]
+            return jugador
+        end
+    end
+
+    # Linea 2
+    if my_array[0][1] != 0 && my_array[1][1] != 0 && my_array[2][1] != 0
+        if my_array[0][1] == my_array[1][1] &&  my_array[1][1] == my_array[2][1]
+            return jugador
+        end
+    end
+
+    # Linea 3
+    if my_array[0][2] != 0 && my_array[1][2] != 0 && my_array[2][2] != 0
+        if my_array[0][2] == my_array[1][2] &&  my_array[1][2] == my_array[2][2]
+            return jugador
+        end
+    end
+
+    #CRUCES
+    #DERECHA
+    if my_array[0][0] != 0 && my_array[1][1] != 0 && my_array[2][2] != 0
+        if my_array[0][0] == my_array[1][1] &&  my_array[1][1] == my_array[2][2]
+            return jugador
+        end
+    end
+
+     #IZQUIERDA
+     if my_array[0][2] != 0 && my_array[1][1] != 0 && my_array[2][0] != 0
+        if my_array[0][2] == my_array[1][1] &&  my_array[1][1] == my_array[2][0]
+            return jugador
+        end
+    end
+end
+
+def tableros(my_array, size, my_reference)
+    puts "\n"
+    puts "-----TABLERO ACTUAL-----"
+
+    for l in 0..my_array.size
+        print my_array[l] 
+        puts "\n"
+    end 
+    
+    #puts i
+    puts "----REFERENCIA----"
+    puts tableroReferencia(size, my_reference)
+    puts "-------------"
+end
+
 for i in 0...9
     if i % 2 == 0
         puts "¿Posición jugador 1?"
@@ -61,6 +142,12 @@ for i in 0...9
             puts "esta posicion ya esta en uso"
         else
             my_array[x][y] = jugador1
+        end
+
+        if evaluarGanador(jugador1, my_array) == jugador1
+            tableros(my_array, size, my_reference)
+            puts "EL GANADOR ES #{jugador1}"
+            break
         end
     end
 
@@ -78,16 +165,13 @@ for i in 0...9
         else
             my_array[x][y] = jugador2
         end
+
+        if evaluarGanador(jugador2, my_array) == jugador2
+            tableros(my_array, size, my_reference)
+            puts "EL GANADOR ES #{jugador2}"
+            break
+        end
     end
 
-    for l in 0..my_array.size
-        print my_array[l] 
-        puts "\n"
-    end 
-    
-    puts i
-end
-
-def evaluarGanador
-    
+    tableros(my_array, size, my_reference)
 end
